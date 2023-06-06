bb36b74dedc87e9da2d931ea574fffe388013bff
shulo-vova <volodya@novisign.com>
2023-06-06 15:21:12 +0300

    update

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -7,4 +7,11 @@ describe('GitHub Actions', () => {
       .find('div.toolbar')
       .should('be.visible')
   })
+
+  it('Everything must work fine', () => {
+    cy.visit('http://localhost:4200')
+      .get('[data-cy-container="application"]')
+      .find('div.toolbar')
+      .should('not.be.visible')
+  })
 })
